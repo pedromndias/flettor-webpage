a90977068eb18e778ac08190c67a8ecafd6504e4
Pedro Dias <pedromndias@gmail.com>
2024-01-13 13:10:35 +0100

    privacy policy updated

--- src/pages/PrivacyPol.jsx
+++ src/pages/PrivacyPol.jsx
@@ -3,7 +3,32 @@ const PrivacyPol = () => {
     <div className="priv-pol">
         <h2>POLÍTICA DE PRIVACIDADE</h2>
         <h3>Última atualização: 12/01/2023</h3>
-        
+        <p>
+        Bem-vindo à Flettor. A sua privacidade é uma prioridade para nós. Esta Política de Privacidade esclarece como tratamos as suas informações pessoais que recolhemos ou que nos são fornecidas através do nosso site.
+        </p>
+        <h3>1. Informações Recolhidas</h3>
+        <p>Quando visita o nosso site, podemos recolher diversas informações, tais como:</p>
+        <ul>
+            <li>Dados de identificação pessoal (nome, endereço de e-mail, contacto telefónico).</li>
+            <li>Dados de navegação, incluindo o seu endereço IP, tipo de navegador utilizado e páginas visitadas.</li>
+        </ul>
+        <h3>2. Utilização das Informações</h3>
+        <p>Utilizamos as informações recolhidas para:</p>
+        <ul>
+            <li>Melhorar a sua experiência no nosso site.</li>
+            <li>Fornecer e otimizar os nossos serviços.</li>
+            <li>Enviar comunicações de marketing, caso tenha dado o seu consentimento.</li>
+        </ul>
+        <h3>3. Proteção das Informações</h3>
+        <p>Implementámos medidas de segurança para proteger as suas informações contra acessos não autorizados, alterações, divulgações ou destruições.</p>
+        <h3>4. Partilha de Informações</h3>
+        <p>Não partilhamos, vendemos ou transferimos as suas informações pessoais a terceiros sem o seu consentimento explícito, salvo quando necessário para cumprir obrigações legais ou fornecer um serviço solicitado.</p>
+        <h3>5. Links para Outros Sites</h3>
+        <p>O nosso site pode conter links para websites de terceiros. Não somos responsáveis pelas políticas de privacidade ou conteúdos desses sites externos.</p>
+        <h3>6. Alterações nesta Política</h3>
+        <p>Reservamo-nos o direito de atualizar ou modificar esta Política de Privacidade a qualquer momento. Aconselhamos que consulte periodicamente esta página para se manter informado.</p>
+        <h3>7. Contacto</h3>
+        <p>Para qualquer questão relacionada com esta Política de Privacidade, por favor, entre em contacto connosco através do nosso e-mail geral@flettor.pt.</p>
     </div>
   )
 }
