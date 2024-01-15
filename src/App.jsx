679b0f3e8ca335d2e78b9371dc0cb7c34339549f
Pedro Dias <pedromndias@gmail.com>
2024-01-15 23:25:58 +0100

    back to top btn adjust

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Solutions from './pages/Solutions'
 import Contact from './pages/Contact'
 import Footer from './components/Footer'
 import TermsCon from './pages/TermsCon'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import backToTop from "./assets/back-to-top.png"
 import PrivacyPol from './pages/PrivacyPol'
 
@@ -16,6 +16,8 @@ function App() {
   
   // Let's create a state to show the HomeCover overlay:
   const [showCover, setShowCover] = useState(true)
+  // And a state to show the back-to-top button:
+  const [showBackToTop, setShowBackToTop] = useState(false);
   const navigate = useNavigate()
 
   const handleEnterClick = () => {
@@ -29,9 +31,23 @@ function App() {
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth"
+      behavior: "auto"
     });
   };
+  useEffect(() => {
+    const handleScroll = () => {
+      // Show back-to-top button when scrolling down, hide when at the top
+      setShowBackToTop(window.scrollY > 100);
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className='app-container'>
@@ -40,7 +56,9 @@ function App() {
 
       <Navbar />
 
-      <button className="back-to-top-btn" onClick={scrollToTop}>
+      <button
+        className={`back-to-top-btn ${showBackToTop ? 'visible' : ''}`}
+        onClick={scrollToTop}>
         <img src={backToTop} alt="back-to-top" />
       </button>
 
